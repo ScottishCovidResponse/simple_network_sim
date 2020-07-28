b5bd2f61f964f56c4682743d0312728b3f660990
bobturneruk <bob.turner.uk@gmail.com>
2020-07-28 13:48:33 +0100

    add ftp credentials

--- scripts/pipeline_upload_output.sh
+++ scripts/pipeline_upload_output.sh
@@ -1,2 +1,15 @@
+token=$1
+access=$2
+username=$3
+password=$4
+
 cd ..
-python -m data_pipeline_api.registry.access_upload --model-config test_pipeline/test_config.yaml --config $2 --remote-uri ssh://boydorr.gla.ac.uk/srv/ftp/scrc/ --submission-script scripts/pipeline_submission.sh --token $1
+
+python -m data_pipeline_api.registry.access_upload \
+'--model-config' 'test_pipeline/test_config.yaml' \
+'--config' $access \
+'--remote-uri' 'ssh://boydorr.gla.ac.uk/srv/ftp/scrc/ ' \
+'--submission-script' 'scripts/pipeline_submission.sh' \
+'--token' $token \
+'--remote-option' 'username' $username \
+'--remote-option' 'password' $password
