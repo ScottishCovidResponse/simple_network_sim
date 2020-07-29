35077017021fe005c69014cc91a12df222619d78
Robert (Bob) Turner <r.d.turner@sheffield.ac.uk>
2020-07-29 10:11:40 +0100

    further parameterise

--- scripts/pipeline_download.sh
+++ scripts/pipeline_download.sh
@@ -1,3 +1,6 @@
+token=$1
+config=$2
+
 cd .. #We're in the scripts/ folder
 
-python -m data_pipeline_api.registry.download --config test_pipeline/test_config.yaml --token $1
+python -m data_pipeline_api.registry.download --config $config --token $token
