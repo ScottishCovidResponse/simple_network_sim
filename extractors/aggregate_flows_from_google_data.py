d25bc123bcfec346d9bcf232ed9b06076a2e5314
Anthony O'Hare <ohare.anthony@gmail.com>
2020-07-30 12:40:40 +0100

    Removed TODOs, tidied up the temporary downloaded files and assumed the iso_3166_2.csv file exists on human/external

--- extractors/aggregate_flows_from_google_data.py
+++ extractors/aggregate_flows_from_google_data.py
@@ -9,7 +9,6 @@ Python requirements:
 - pathlib
 - pandas
 - zipfile
-- ftplib.FTP  ??? Only if the hand generated lookup table is hosted on the FTP server.
 - urllib.request
 - data_pipeline_api
 
@@ -55,7 +54,6 @@ def download_pop_table():
 
     population_table = "mid-year-pop-est-18-tabs_Table 2.csv"
 
-
     # If the population table doesn't exist download it.
     if not Path(data_path / population_table).exists():
         print(f"Could not find {data_path}/{population_table}, downloading it")
@@ -79,6 +77,9 @@ def download_pop_table():
 
     total_population =  dfPop['population'].sum()
 
+    # clean up (i.e. remove) the downloaded datafile(s)
+    Path(data_path / population_table).unlink(missing_ok=False)
+
     return dfPop, total_population
 
 def download_lookup_table():
@@ -88,70 +89,22 @@ def download_lookup_table():
     :return: A dataframe containing the full ISO code (GB-iso_3166_2) and the corresponding local authority code.
     """
     
-    #TODO - should I check if these files are in the pipeline and if not upload them before starting?
-    # If so - I don't know how to write the metadata…yaml file stuff
-
     # ISO region to LA best-attempt lookup table: compiled by hand, Jess Enright, 30 June 2020
     lookup_table = "iso-3166-2_to_scottishLA.csv"
 
-    """
-    TODO - Question: Do we want this file to be saved somewhere, or save the data here as a dataframe?
-    if <download_lookup_from ftp_server>:
-         dfLookup = pd.read_csv(lookup_table, low_memory=False)
-    else:
-        # ISO region to LA best-attempt lookup table: compiled by hand, Jess Enright, 30 June 2020
-        data = [['ABE','Aberdeen','S12000033','Aberdeen City'],
-['ABD','Aberdeenshire','S12000034','Aberdeenshire'],
-['ANS','Angus','S12000041','Angus'],
-['AGB','Argyll and Bute','S12000035','Argyll and Bute'],
-['EDH','Edinburgh','S12000036','City of Edinburgh'],
-['CLK','Clackmannanshire','S12000005','Clackmannanshire'],
-['DGY','Dumfries and Galloway','S12000006','Dumfries and Galloway'],
-['DND','Dundee','S12000042','Dundee City'],
-['EAY','East Ayrshire','S12000008','East Ayrshire'],
-['EDU','East Dunbartonshire','S12000045','East Dunbartonshire'],
-['ELN','East Lothian','S12000010','East Lothian'],
-['ERW','East Renfrewshire','S12000011','East Renfrewshire'],
-['FAL','Falkirk','S12000014','Falkirk'],
-['FIF','Fife','S12000047','Fife'],
-['GLG','Glasgow','S12000046','Glasgow City'],
-['HLD','Highland','S12000017','Highland'],
-['IVC','Inverclyde','S12000018','Inverclyde'],
-['MLN','Midlothian','S12000019','Midlothian'],
-['MRY','Moray','S12000020','Moray'],
-['ELS','Eilean Siar','S12000013','Na h-Eileanan Siar'],
-['NAY','North Ayrshire','S12000021','North Ayrshire'],
-['NLK','North Lanarkshire','S12000044','North Lanarkshire'],
-['ORK','Orkney Islands','S12000023','Orkney Islands'],
-['PKN','Perth and Kinross','S12000048','Perth and Kinross'],
-['RFW','Renfrewshire','S12000038','Renfrewshire'],
-['SCB','Scottish Borders The','S12000026','Scottish Borders'],
-['ZET','Shetland Islands','S12000027','Shetland Islands'],
-['SAY','South Ayrshire','S12000028','South Ayrshire'],
-['SLK','South Lanarkshire','S12000029','South Lanarkshire'],
-['STG','Stirling','S12000030','Stirling'],
-['WDU','West Dunbartonshire','S12000039','West Dunbartonshire'],
-['WLN','West Lothian','S12000040','West Lothian']]
-        dfLookup = pd.DataFrame(data,columns=['iso_3166_2','name_of_subdivision','la_code','la_name'])
-        dfLookup.set_index('iso_3166_2',inplace = True)
-        dfLookup.to_csv('iso_codes.csv')
-        #upload dfLookup.to_csv() to server
-    """
-
-    dfLookup = pd.read_csv(lookup_table, low_memory=False)
-    dfLookup['full_iso_code'] = "GB-" + dfLookup.iso_3166_2
-    codes_of_interest = list(dfLookup['full_iso_code'])
-    dfLookup = dfLookup[['full_iso_code', 'la_code']]
-
-    # clean up (i.e. remove) the downloaded datafile(s)
-    #Path(lookup_table).unlink(missing_ok=False)
+    with DataProcessingAPI(data_path / config_filename, uri=uri, git_sha=git_sha) as api:
+        with api.read_external_object(lookup_table) as file:
+            dfLookup = pd.read_csv(file, low_memory=False)
+            dfLookup['full_iso_code'] = "GB-" + dfLookup.iso_3166_2
+            codes_of_interest = list(dfLookup['full_iso_code'])
+            dfLookup = dfLookup[['full_iso_code', 'la_code']]
 
     return dfLookup, codes_of_interest
 
 def download_google_mogility_data(la_list):
     """
     Downloads the mobility data from google for Scottish local authorities if it doesn't exists in data_path
-    :param: The list of local åuthorities for which we want the movement data
+    :param: The list of local authorities for which we want the movement data
     :return: A dataframe containing the full ISO code (GB-iso_3166_2) and the corresponding local authority code
     """
 
@@ -167,8 +120,11 @@ def download_google_mogility_data(la_list):
 
     with DataProcessingAPI(config_filename, uri=uri, git_sha=git_sha) as api:
         with api.read_external_object(google_mobility_table) as file:
-            justScotGoogle = pd.read_csv(google_mobility_table, low_memory=False)
+            justScotGoogle = pd.read_csv(file, low_memory=False)
             justScotGoogle = justScotGoogle[justScotGoogle['iso_3166_2_code'].isin(la_list) == True]
+
+    # clean up (i.e. remove) the downloaded datafile(s)
+    Path( data_path / google_mobility_table).unlink(missing_ok=False)
 
     return justScotGoogle
 
@@ -205,14 +161,18 @@ def main():
     dfScotGoogle = dfScotGoogle[dfScotGoogle.index.dayofweek <5]
 
 
-    # TODO - upload this to the database; human/movement-multipliers/1/data.h5
-    dfScotGoogle.to_csv('movement_multiplier.csv')
+    # Now upload this to the database; human/movement-multipliers/1/data.h5
+    movement_multiplier_table = "movement_multiplier.csv"
+    dfScotGoogle.to_csv(movement_multiplier_table)
     with DataProcessingAPI(config_filename, uri=uri, git_sha=git_sha) as api:
         api.write_table(
             "generated_sns_products/movement_multiplier",
             "movement_multiplier",
             dfScotGoogle,
             )    
+
+    # clean up (i.e. remove) the downloaded datafile(s)
+    Path(movement_multiplier_table).unlink(missing_ok=False)
 
 if __name__ == "__main__":
     main()
