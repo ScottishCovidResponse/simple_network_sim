ee1ec572d04c740958c03b393a53e0107189712d
Robert (Bob) Turner <r.d.turner@sheffield.ac.uk>
2020-07-30 14:59:11 +0100

    move towards working with v0.7.1 data api

--- scripts/pipeline_upload_output.sh
+++ scripts/pipeline_upload_output.sh
@@ -8,8 +8,6 @@ cd ..
 python -m data_pipeline_api.registry.access_upload \
 '--model-config' 'test_pipeline/test_config.yaml' \
 '--config' $access \
-'--remote-uri' 'ssh://boydorr.gla.ac.uk/srv/ftp/scrc/' \
-'--remote-uri-override' 'ftp://boydorr.gla.ac.uk/scrc/' \
 '--submission-script' 'scripts/pipeline_submission.sh' \
 '--token' $token \
 '--remote-option' 'username' $username \
