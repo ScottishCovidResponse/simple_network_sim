c580c60e99eba12f2964241260db81176128089e
Jess Enright <jenright@gmail.com>
2020-08-31 07:15:49 +0100

    more work on basic runs

--- simpler_education_runs.py
+++ simpler_education_runs.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import networkx as nx
+import matplotlib.pyplot as plt
 
 from simple_network_sim import network_of_populations as ss
 from simple_network_sim.sampleUseOfModel import runSimulation, aggregateResults
@@ -24,38 +25,73 @@ def generate_education_network_and_pop(bubble_size=24, number_bubbles = 100, bet
         
     return population, connections
 
-pop, conn = generate_education_network_and_pop(bubble_size=50, number_bubbles = 4, between_bubble_prob=0.5)
-transitionRates = pd.read_csv('education_input/compartment_transition_rates_for_education.csv')
-mixing_matrix = pd.read_csv('education_input/education_mixing_matrix.csv')
-infections_comps = pd.read_csv("education_input/infectious_comps.csv")
-infection_prob = pd.read_csv("education_input/infection_prob.csv")
-initial_infection = pd.read_csv('education_input/education_initial_infections.csv')
-# trials = pd.read_csv("education_input/trials.csv")
-trials = pd.DataFrame([1], columns=["Value"])
-dates = pd.read_csv("education_input/start_end.csv")
-stoch = pd.read_csv("education_input/stochastic_mode.csv")
-print(trials)
+def plot_for_testing(df, filename):
+    # df['date'] = pd.to_datetime(df['date'])    
+    for state in ['S', 'E', 'A', 'I', 'R', 'P_T', 'P_F']:
+    # for state in set(df['state']):
+        thisDF = df[df.state == state][['date', 'mean']]
+        plt.plot(range(len(list(thisDF['mean']))), list(thisDF['mean']), label=state)
+    plt.legend()
+    plt.savefig(filename)
+        
 
+pop, conn = generate_education_network_and_pop(bubble_size=20, number_bubbles = 100, between_bubble_prob=0.1)
 
-network, new_issues = ss.createNetworkOfPopulation(
-    transitionRates,
-    pop,
-    conn,
-    mixing_matrix,
-    infections_comps,
-    infection_prob,
-    initial_infection,
-    trials,
-    dates,
-    None,
-    stoch)
-
-print(network)
-
-
-results = runSimulation(network, 143, [])
-
-
-res_agg = aggregateResults(results)
-res_agg[0].to_csv('just_for_jess.csv')
-print(res_agg)
+for test_rate in [0.01, 0.05, 0.1]:
+    
+    
+    prestring = 'test_rate-' + str(test_rate)
+    
+    transitionRates = pd.read_csv('education_input/compartment_transition_rates_for_education_rounded_test_rate-' + str(test_rate) +'.csv')
+    print(transitionRates)
+    print('\n\n\n')
+    
+    mixing_matrix = pd.read_csv('education_input/education_mixing_matrix.csv')
+    infections_comps = pd.read_csv("education_input/infectious_comps.csv")
+    infection_prob = pd.read_csv("education_input/infection_prob.csv")
+    initial_infection = pd.read_csv('education_input/education_initial_infections.csv')
+    # trials = pd.read_csv("education_input/trials.csv")
+    trials = pd.DataFrame([1], columns=["Value"])
+    dates = pd.read_csv("education_input/start_end.csv")
+    stoch = pd.read_csv("education_input/stochastic_mode.csv")
+    falsePos = 0.023*test_rate
+    # print(trials)
+    
+    
+    network, new_issues = ss.createNetworkOfPopulation(
+        transitionRates,
+        pop,
+        conn,
+        mixing_matrix,
+        infections_comps,
+        infection_prob,
+        initial_infection,
+        trials,
+        dates,
+        None,
+        stoch,
+        falsePos)
+    
+    # print(network)
+    
+    
+    results = runSimulation(network, 143, [])
+    
+    
+    res_agg = aggregateResults(results)
+    res_agg[0].to_csv(prestring + 'just_for_jess.csv')
+    
+    forcheck = res_agg[0][['date', 'state', 'mean']]
+    
+    sanity_check = forcheck[['date', 'mean']].groupby(['date']).sum()
+    print(sanity_check)
+    
+    # print (forcheck)
+    forcheck = forcheck.groupby(['date', 'state']).sum()
+    # print(forcheck)
+    forcheck.to_csv(prestring + 'summary_for_jess.csv')
+    forcheck = pd.read_csv(prestring + 'summary_for_jess.csv')
+    plt.clf()
+    plot_for_testing(forcheck, prestring + '_basic_output.pdf')
+    
+    print(res_agg)
